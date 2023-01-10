bacfcff6278feec4447718f2cf0a440016925b2a
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-01-10 15:14:13 +0300

    Search Views etc

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('category/<slug:slug>/', views.get_category, name='category'),
-    path('post/<slug:slug>/', views.get_post, name='post')
+    path('category/<slug:slug>/', views.PostsbyCategory.as_view(), name='category'),
+    path('tag/<slug:slug>/', views.PostsbyTag.as_view(), name='tag'),
+    path('post/<slug:slug>/', views.GetPost.as_view(), name='post'),
+    path('search/', views.Search.as_view(), name='search')
 ]
